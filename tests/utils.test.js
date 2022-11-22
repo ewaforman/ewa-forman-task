967b06a30fc1f8b36b0f38c05564e8e2d4b87825
Ewa Forman <ewaforman@gmail.com>
2022-11-22 22:10:27 +0100

    hometask solution - fixed

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,5 @@
 import { getRectanglePerimeter, getRectangleArea, getRectangleInfo } from "../js/rectangle";
+import {jest} from '@jest/globals';
 
 test('should properly output rectangle perimeter', () => {
     const output = getRectanglePerimeter(2, 3);
@@ -11,7 +12,8 @@ test('should properly output rectangle area', () => {
     expect(output).toBe(6);
 });
 
-test ('should generate a valid text output', () => {
-    const output = getRectangleInfo(2, 3);
-    expect(output).toBe('The perimeter of a rectangle is 10 and the area is 6');
+test('should generate a valid text output', () => {  
+    console.log = jest.fn()
+    getRectangleInfo(2, 3);
+    expect(console.log).toHaveBeenCalledWith('The perimeter of a rectangle is 10 and the area is 6');    
 });
